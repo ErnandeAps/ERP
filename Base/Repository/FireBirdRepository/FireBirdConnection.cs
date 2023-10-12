fa28bf6ef7b0d6b1e2dd977a5e6e4a7306db96b6
Felipe santana batista dos santos <felipe-sbatista@outlook.com>
2023-10-12 20:33:25 -0300

    add db

---
+++ Base/Repository/FireBirdRepository/FireBirdConnection.cs
@@ -0,0 +1,19 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Base.Funcoes;
+
+public class FireBirdConnection
+{
+    public FbConnection getConexao()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+        var configuration = builder.Build();
+
+
+        string conn = configuration.GetSection("FireBirdConnectionString")?.Value;
+        return new FbConnection(conn);
+    }
+}
